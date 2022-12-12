a8f7630b609fcbe70df0f8f90c2a83c6e251f574
Cory Sougstad <cory@hau.me>
2022-12-12 13:30:54 -0700

    Update functions. Hard coded decisions

--- day2/part2.ts
+++ day2/part2.ts
@@ -1,123 +1,161 @@
-import strategyGuide from "input"; 
-
-enum RoundChoice {
-    Rock = "A",
-    Paper = "B",
-    Scissors = "C",
-}
-
-enum DesiredOutcome {
-    Lose = "X",
-    Draw = "Y",
-    Win = "Z",
-}
-
-type RoundStrategy = {
-    elfChoice: RoundChoice;
-    desiredOutcome: DesiredOutcome;
-}
-
-type Round = {
-  elfChoice: RoundChoice;  
-  myChoice: RoundChoice;
-}
-
-const choiceScoreMatrix = {
-    [RoundChoice.Rock]: 1,
-    [RoundChoice.Paper]: 2,
-    [RoundChoice.Scissors]: 3,
-}
-
-// TBD
-const outcomeScoreMatrix = {
-    
-}
-
 /**
  * Score:
  * Choice
  * 1 - Rock
  * 2 - Paper
  * 3 - Scissors
- * 
+ *
  * Outcome
  * 0 - Loss
  * 3 - Tie
  * 6 - Win
+ *
+ * rock beats scissors
+ * paper beats rock
+ * scissors beats paper
+ * r,1 + s,4 = 5 - win
+ * p,2 + r,1 = 3 - win
+ * s,4 + p,2 = 6 - win
  */
 
-// format the strategyGuide input into a useable state
+// import raw data
+import strategyGuide from "input";
+
+// enum for choices based on elf input
+enum RoundChoice {
+  Rock = "A",
+  Paper = "B",
+  Scissors = "C",
+}
+
+// conversion table from choice to int for scoring
+const choiceInt = {
+  [RoundChoice.Rock]: 1,
+  [RoundChoice.Paper]: 2,
+  [RoundChoice.Scissors]: 4,
+};
+
+// enum for desired outcome
+enum DesiredOutcome {
+  Lose = "X",
+  Draw = "Y",
+  Win = "Z",
+}
+
+// type for strategy guide records
+type RoundStrategy = {
+  elfChoice: RoundChoice;
+  desiredOutcome: DesiredOutcome;
+};
+
+// type for round records
+type Round = {
+  elfChoice: RoundChoice;
+  myChoice: RoundChoice;
+};
+
+// score each choice gives you regardless of win or lose
+const choiceScoreMatrix = {
+  [RoundChoice.Rock]: 1,
+  [RoundChoice.Paper]: 2,
+  [RoundChoice.Scissors]: 3,
+};
+
+/**
+ * format the strategyGuide input into a useable state
+ * @returns {RoundStrategy[]} `RoundStrategy[]`
+ **/
 const formatStrategyGuide = (): RoundStrategy[] => {
-    return strategyGuide.split("\n").map((round) => {
-        const roundAry = round.split(" ");
-        return {
-            elfChoice: roundAry[0] as RoundChoice,
-            desiredOutcome: roundAry[1] as DesiredOutcome,
-        } as const
-    });
-}
+  return strategyGuide.split("\n").map((round) => {
+    const roundAry = round.split(" ");
+    return {
+      elfChoice: roundAry[0] as RoundChoice,
+      desiredOutcome: roundAry[1] as DesiredOutcome,
+    } as const;
+  });
+};
 
-// calculate the choice we need to make to meet the strategy guide condition
-const calculateMyChoice = (roundStrat: RoundStrategy): RoundChoice => {
-  switch(JSON.stringify(roundStrat)) {
-    case JSON.stringify({elfChoice: RoundChoice.Rock, desiredOutcome: DesiredOutcome.Win}):
-      return RoundChoice.Paper;
-    case JSON.stringify({elfChoice: RoundChoice.Rock, desiredOutcome: DesiredOutcome.Draw}):
-        return RoundChoice.Rock;
-    case JSON.stringify({elfChoice: RoundChoice.Rock, desiredOutcome: DesiredOutcome.Lose}):
-      return RoundChoice.Scissors;
-    case JSON.stringify({elfChoice: RoundChoice.Paper, desiredOutcome: DesiredOutcome.Win}):
-      return RoundChoice.Scissors;
-    case JSON.stringify({elfChoice: RoundChoice.Paper, desiredOutcome: DesiredOutcome.Draw}):
-        return RoundChoice.Paper;
-    case JSON.stringify({elfChoice: RoundChoice.Paper, desiredOutcome: DesiredOutcome.Lose}):
-      return RoundChoice.Rock;
-    case JSON.stringify({elfChoice: RoundChoice.Scissors, desiredOutcome: DesiredOutcome.Win}):
-      return RoundChoice.Rock;
-    case JSON.stringify({elfChoice: RoundChoice.Scissors, desiredOutcome: DesiredOutcome.Draw}):
-        return RoundChoice.Scissors;
-    case JSON.stringify({elfChoice: RoundChoice.Scissors, desiredOutcome: DesiredOutcome.Lose}):
-      return RoundChoice.Paper;
+/**
+ * Calculate the choice I should make to get the desired win condition based on the elf's move
+ * adding numbers only duplicates in draw conditions
+ * @param {RoundChoice} c1
+ * @param {RoundChoice} c2
+ * @returns {number} score
+ */
+const calculateMyChoiceV2 = (roundStrat: RoundStrategy) => {
+  // if same, return score for draw
+  if (roundStrat.desiredOutcome === DesiredOutcome.Draw)
+    return roundStrat.elfChoice;
+  switch (roundStrat.elfChoice) {
+    case RoundChoice.Rock:
+      return roundStrat.desiredOutcome === DesiredOutcome.Win
+        ? RoundChoice.Paper
+        : RoundChoice.Scissors;
+    case RoundChoice.Paper:
+      return roundStrat.desiredOutcome === DesiredOutcome.Win
+        ? RoundChoice.Scissors
+        : RoundChoice.Rock;
+    case RoundChoice.Scissors:
+      return roundStrat.desiredOutcome === DesiredOutcome.Win
+        ? RoundChoice.Rock
+        : RoundChoice.Paper;
   }
-}
+};
 
-const roundScore = (round: Round): number => {
-    switch(JSON.stringify(round)) {
-        case JSON.stringify({myChoice: RoundChoice.Rock, elfChoice: RoundChoice.Scissors}):
-            return 6;
-        case JSON.stringify({myChoice: RoundChoice.Rock, elfChoice: RoundChoice.Rock}):
-            return 3;
-        case JSON.stringify({myChoice: RoundChoice.Rock, elfChoice: RoundChoice.Paper}):
-            return 0;
-        case JSON.stringify({myChoice: RoundChoice.Paper, elfChoice: RoundChoice.Scissors}):
-            return 0;
-        case JSON.stringify({myChoice: RoundChoice.Paper, elfChoice: RoundChoice.Rock}):
-            return 6;
-        case JSON.stringify({myChoice: RoundChoice.Paper, elfChoice: RoundChoice.Paper}):
-            return 3;
-        case JSON.stringify({myChoice: RoundChoice.Scissors, elfChoice: RoundChoice.Scissors}):
-            return 3;
-        case JSON.stringify({myChoice: RoundChoice.Scissors, elfChoice: RoundChoice.Rock}):
-            return 0;
-        case JSON.stringify({myChoice: RoundChoice.Scissors, elfChoice: RoundChoice.Paper}):
-            return 6;
-        default:
-            throw new Error(`No match found: ${JSON.stringify(round)}`);
-    }
-}
+/**
+ * Calculate the score for the round
+ * Adding numbers only duplicates in draw conditions
+ * Adding the choices after removing draws gives unique numbers for each outcome
+ * Then we just have to see which one made the winning choice and return the score
+ * @param {Round} round
+ * @returns {number} score
+ */
+const roundScoreV2 = (round: Round) => {
+  // if same, return score for draw
+  if (round.myChoice === round.elfChoice) return 3;
+  // based on the winner, return the score
+  switch (choiceInt[round.myChoice] + choiceInt[round.elfChoice]) {
+    case 3:
+      return round.myChoice === RoundChoice.Paper ? 6 : 0;
+    case 5:
+      return round.myChoice === RoundChoice.Rock ? 6 : 0;
+    case 6:
+      return round.myChoice === RoundChoice.Scissors ? 6 : 0;
+    default:
+      throw new Error(
+        `No match found: ${JSON.stringify(round)}, ${
+          choiceInt[round.myChoice] + choiceInt[round.elfChoice]
+        }`
+      );
+  }
+};
 
-// calculate score for round
+/**
+ * Calculate score for round
+ * First get the choice score
+ * Then calculate the outcome (win/loss/draw) score
+ * Finally add up the totals and return
+ * @param {Round} round
+ * @returns {number} score [0, 3, 6]
+ */
 const scoreForRound = (round: Round): number => {
-    const choiceScore = choiceScoreMatrix[round.myChoice];
-    const outcomeScore = roundScore(round);
-    const totalScore = choiceScore + outcomeScore;
-    return totalScore;
-}
+  const choiceScore = choiceScoreMatrix[round.myChoice];
+  const outcomeScore = roundScoreV2(round);
+  const totalScore = choiceScore + outcomeScore;
+  return totalScore;
+};
 
-
-// run
+// Use the functions to calculate the final score
+// format the raw input
 const formattedStrategyGuide = formatStrategyGuide();
-const rounds = formattedStrategyGuide.map((round) => ({...round, myChoice: calculateMyChoice(round)} as Round));
-const roundScores = rounds.map((round) => scoreForRound({myChoice: round.myChoice, elfChoice: round.elfChoice}));
+// calculate the required input for each round
+const rounds = formattedStrategyGuide.map(
+  (round) => ({ ...round, myChoice: calculateMyChoiceV2(round) } as Round)
+);
+// calculate the score for each round
+const roundScores = rounds.map((round) =>
+  scoreForRound({ myChoice: round.myChoice, elfChoice: round.elfChoice })
+);
+// sum the scores
 const finalScore = roundScores.reduce((prev, curr) => prev + curr);
 console.log(finalScore);
